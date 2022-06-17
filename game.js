51ce46b77312246aaa402f09ad329b53ef5cc2b4
mariana ferro <marianafe00@gmail.com>
2022-06-17 17:49:41 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
+var gameSettings = {
+	playerSpeed: 200,
+}
+
 var config = {
 	width: 640,
 	height: 640,
 	backgroundColor: 0x000000,
-	scene: [Boot, Load, Menu, LvlOne]
+	scene: [Boot, Load, Menu, LvlOne],
+	pixelArt:true,
+	physics: {
+		default: "arcade", 
+		arcade:{
+			debug: false
+		}
+	}
 }
 
 window.onload = function (){
 	var game = new Phaser.Game(config);
-	//game.state.add('Boot', bootGame);
-	//game.state.start('Boot')
 }
