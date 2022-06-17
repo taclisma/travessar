f0ea401462a95c80845296ad4cc276b43a465275
mariana ferro <marianafe00@gmail.com>
2022-06-17 17:49:58 -0300

    Update LvlOne.js

--- LvlOne.js
+++ LvlOne.js
@@ -9,13 +9,17 @@ class LvlOne extends Phaser.Scene{
         
         this.carro2 = this.add.image(config.width/2, config.height/2, "carro");
 
-        this.player = this.add.image(config.width/2, config.height - 16, "player");
+        this.player = this.physics.add.image(config.width/2, config.height - 16, "player");
         this.player.setOrigin(0.5,1);
+
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
     }
     
     update(){
         this.moveCarro(this.carro, 3)
         this.moveCarro(this.carro2, 4)
+
+        this.movePlayerManager();
     }
 
     //funçao p fazer o obj andar
@@ -35,4 +39,19 @@ class LvlOne extends Phaser.Scene{
         carro.y = randomY;
     }
 
+    movePlayerManager(){
+        if(this.cursorKeys.up.isDown){
+            this.player.setVelocityY(-gameSettings.playerSpeed);
+        }else if(this.cursorKeys.down.isDown){
+            this.player.setVelocityY(+gameSettings.playerSpeed);
+        }else if(this.cursorKeys.left.isDown){
+            this.player.setVelocityX(-gameSettings.playerSpeed);
+        }else if(this.cursorKeys.right.isDown){
+            this.player.setVelocityX(+gameSettings.playerSpeed);   
+        } else {
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+        }
+    }
 }
+
