4bb03f9e9681d51f69ce928d7833e426781ba269
mariana ferro <marianafe00@gmail.com>
2022-06-27 10:47:36 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 var gameSettings = {
 	playerSpeed: 48,
+	lanes: [100, 200, 300, 400, 500]
 }
 
+// isso pode ir p/ scene load
+function enemyRndPos(){
+	let randomY = Phaser.Math.RND.pick(gameSettings.lanes);
+	return randomY;
+}
 var config = {
-	width: 640,
-	height: 640,
+	width: 600,
+	height: 600,
 	backgroundColor: 0x000000,
 	scene: [Boot, Load, Menu, LvlOne],
 	pixelArt:true,
