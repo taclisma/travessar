7a5581f6d1c4c1a91e75091a9c9e873b3e1f2cf4
mariana ferro <marianafe00@gmail.com>
2022-07-08 16:13:28 -0300

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -6,17 +6,19 @@ class Menu extends Phaser.Scene{
     create(){
         this.bg = this.add.tileSprite(0,0, config.width, config.height, "bg");
         this.bg.setOrigin(0,0);
+        this.bg.setTileScale(1.28, 1.28);
+
         
-        this.bt = this.add.sprite(320, 320, "bt");
+        this.bt = this.add.sprite(config.width/2, config.height/2, "bt");
         this.anims.create({
             key: "playAnim",
             frames: this.anims.generateFrameNumbers("bt"),
             frameRate: 5,
             repeat: -1
         });
+        
         this.bt.setInteractive();
         this.bt.play("playAnim");
-
         this.input.on('gameobjectdown', this.menuPlay, this);
     }
 
